422d68f1ba86e6d13ef6cd459c22fd0ea2059af3
Carole <cjpillette@hotmail.com>
2017-04-19 14:10:33 +1000

    implemented rudimental navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_up: 'register', sign_out: 'logout'}
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   resources :blogs
   resources :portfolios
 
