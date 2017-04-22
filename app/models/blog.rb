266b576072537505ce5483e7fd9d84294efd258c
Carole <cjpillette@hotmail.com>
2017-04-22 15:43:40 +1000

    integrated nested attributes for blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
 
   belongs_to :topic
+  accepts_nested_attributes_for :topic, reject_if: lambda { |attrs| attrs['title'].blank? }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+  title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics were created"
+
 5.times do |blog|
   Blog.create!(
   title: "Blog title: #{blog}",
-  body: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+  body: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  topic_id: Topic.last.id
   )
 end
 
