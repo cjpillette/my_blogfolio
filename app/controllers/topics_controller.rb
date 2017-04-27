b19a342ec58a270907fba47d75848e4a09d03641
Carole <cjpillette@hotmail.com>
2017-04-27 12:04:39 +1000

    created a CRUD for Topic

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,74 @@
+class TopicsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  layout "blog"
+
+  # GET /blogs
+  # GET /blogs.json
+  def index
+    @topics = Topic.unique_title
+  end
+
+
+  def show
+  end
+
+  # GET /blogs/new
+  def new
+    @topic = Topic.new
+  end
+
+  # GET /blogs/1/edit
+  def edit
+  end
+
+  # POST /blogs
+  # POST /blogs.json
+  def create
+    @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+        format.json { render :show, status: :created, location: @topic }
+      else
+        format.html { render :new }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /blogs/1
+  # PATCH/PUT /blogs/1.json
+  def update
+    respond_to do |format|
+      if @topic.update(topic_params)
+        format.html { redirect_to @topic, notice: 'Topic was successfully updated.' }
+        format.json { render :show, status: :ok, location: @topic }
+      else
+        format.html { render :edit }
+        format.json { render json: @topic.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /blogs/1
+  # DELETE /blogs/1.json
+  def destroy
+    @topic.destroy
+    respond_to do |format|
+      format.html { redirect_to topics_url, notice: 'Topic was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_blog
+      @topic = Topic.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def topic_params
+      params.require(:topic).permit(:title)
+    end
+end
