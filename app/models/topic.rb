ac0920b07c29856d8464276d6c8a574e5e4b60ba
Carole <cjpillette@hotmail.com>
2017-05-15 12:14:33 +1000

    can delete topic fromm blog now

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
   validates_presence_of :title
 
-  has_many :blogs
+  has_many :blogs, dependent: :nullify
 
   # scope :unique_title, -> { Topic.uniq.pluck(:title)}
   mount_uploader :avatar, AvatarUploader
